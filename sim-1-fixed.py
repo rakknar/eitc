split sim.py   into a fixed file to run first   and an experimental file to futz with interactively fix a bug: corners and max_payout should be expressed in thousands, not units

sim-1-fixed.py
@@ -3,26 +3,7 @@ import numpy as np
 import math as math
 
 
-if True: # set some parameters
-  if True: # parameters of the EITC
-    corner_1 = 300 # income level where the first corner lies
-    corner_2 = 600
-    corner_3 = 1200
-    max_payout = 300 # max payout
-
-  if True: # 2016 poverty lines
-    poverty =         241673
-    extreme_poverty = 114692
-
-if True: # The data
-  raw = pd.read_csv("data/2016-wages.csv").rename(columns = {"w_m_gross":"wage_g_m", "profit":"profit_g_m"} )
-  ppl = raw.copy()[['wage_g_m', 'profit_g_m', 'hh_id1', 'hh_id2']]
-    # ppl drops from raw the hours variables, among others
-
-  # "wage" and "profit" in this data are mutually exclusive.
-  ppl["wage_g_m"].fillna(0, inplace=True)      # wage
-  ppl["profit_g_m"].fillna(0, inplace=True)    # non-wage
-  ppl["income_g_m"] = ppl["profit_g_m"] + ppl["wage_g_m"]
+raw = pd.read_csv("data/2016-wages.csv").rename(columns = {"w_m_gross":"wage_g_m", "profit":"profit_g_m"} )
 
 def add_eitc_columns (df, income_colname, corner_1, corner_2, corner_3, max_payout):
   x = df[income_colname]
@@ -44,11 +25,6 @@ def add_eitc_columns (df, income_colname, corner_1, corner_2, corner_3, max_payo
 
   return(df, cost_in_trillions)
 
-ppl,cost = add_eitc_columns( ppl, "income_g_m", corner_1, corner_2, corner_3, max_payout )
-
-hhs = ( ppl.groupby(['hh_id1', 'hh_id2'])
-        [["income_g_m", "income+eitc"]].agg('sum') )
-
 def compute_poverty_gap_change (df, income_0_colname, income_1_colname):
   x = df[income_0_colname]
   df["poverty_0"]         = np.where( x < poverty        , poverty - x        , 0 )
@@ -62,9 +38,3 @@ def compute_poverty_gap_change (df, income_0_colname, income_1_colname):
   df["extreme_poverty_drop"] = df["extreme_poverty_0"] - df["extreme_poverty_1"]
 
   return ( df, df["poverty_drop"].sum(), df["extreme_poverty_drop"].sum() )
-
-hhs, drop, extreme_drop = compute_poverty_gap_change( hhs, "income_g_m", "income+eitc" )
-
-pd.set_option('display.max_columns', 20)
-x = hhs[ (hhs["extreme_poverty_0"] > 0) & (hhs["income_g_m"] > 0) ]
-x[['income_g_m', 'income+eitc', 'poverty_0', 'poverty_1', 'poverty_drop']]