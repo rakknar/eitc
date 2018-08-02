dfa54f34577ddcfa0515c00ee3b6a022b5e51632
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2018-08-02 17:40:56 -0500

    compute poverty exits add some tests scale results for readability shift code for readability (parallelism)

--- python/sim-1-fixed.py
+++ python/sim-1-fixed.py
@@ -5,6 +5,9 @@ import math as math
 
 raw = pd.read_csv("data/2016-wages.csv").rename(columns = {"w_m_gross":"wage_g_m", "profit":"profit_g_m"} )
 
+scale_to_population = 49e6 / len(raw) # TODO ? is ratio of individuals, could be different for households
+
+
 def add_eitc_columns (df, income_colname, corner_1, corner_2, corner_3, max_payout):
   x = df[income_colname]
   df["seg-1"] = np.where((x > 0)          & (x < corner_1),
@@ -19,22 +22,35 @@ def add_eitc_columns (df, income_colname, corner_1, corner_2, corner_3, max_payo
   df["eitc"] = df["seg-1"] + df["seg-2"] + df["seg-3"]
   df["income+eitc"] = df[income_colname] + df["eitc"] # post-eitc income
 
-  cost_in_trillions = ( df["eitc"].sum()           # scale to actual population size, 49 million
-                        * (49e6 / len(df["eitc"])) # add every eitc expenditure
+  cost_in_trillions = ( df["eitc"].sum()           # add every eitc expenditure
+                        * scale_to_population      # scale to actual population size
                         / 1e12 )                   # express in trillions of pesos
 
   return(df, cost_in_trillions)
 
-def compute_poverty_gap_change (df, income_0_colname, income_1_colname):
+
+def add_poverty_gap_change (df, income_0_colname, income_1_colname):
   x = df[income_0_colname]
-  df["poverty_0"]         = np.where( x < poverty        , poverty - x        , 0 )
+  df[        "poverty_0"] = np.where( x <         poverty,         poverty - x, 0 )
   df["extreme_poverty_0"] = np.where( x < extreme_poverty, extreme_poverty - x, 0 )
 
   x = df[income_1_colname]
-  df["poverty_1"]         = np.where( x < poverty        , poverty - x        , 0 )
+  df[        "poverty_1"] = np.where( x <         poverty,         poverty - x, 0 )
   df["extreme_poverty_1"] = np.where( x < extreme_poverty, extreme_poverty - x, 0 )
 
-  df["poverty_drop"]         = df["poverty_0"]         - df["poverty_1"]
+  df[        "poverty_drop"] = df[        "poverty_0"] - df[        "poverty_1"]
   df["extreme_poverty_drop"] = df["extreme_poverty_0"] - df["extreme_poverty_1"]
 
-  return ( df, df["poverty_drop"].sum(), df["extreme_poverty_drop"].sum() )
+  poverty_drop         = (scale_to_population / 1e9) * df[        "poverty_drop"].sum()
+  extreme_poverty_drop = (scale_to_population / 1e9) * df["extreme_poverty_drop"].sum()
+
+  return ( df, poverty_drop, extreme_poverty_drop )
+
+
+def add_poverty_exits (df, income_0_colname, income_1_colname):
+  inc0, inc1 = income_0_colname, income_1_colname
+  df[        "poverty_exit"] = (df[inc0] <         poverty) & (df[inc1] >         poverty)
+  df["extreme_poverty_exit"] = (df[inc0] < extreme_poverty) & (df[inc1] > extreme_poverty)
+  poverty_exits         = scale_to_population * df[        "poverty_exit"].sum()
+  extreme_poverty_exits = scale_to_population * df["extreme_poverty_exit"].sum()
+  return ( df, poverty_exits, extreme_poverty_exits )
--- python/sim-2-interactive.py
+++ python/sim-2-interactive.py
@@ -25,14 +25,18 @@ if True: # manicure the data
 
 
 if True: # results
-  ppl,cost = add_eitc_columns( ppl, "income_g_m", corner_1, corner_2, corner_3, max_payout )
+  ppl,cost_in_trillions = add_eitc_columns( ppl, "income_g_m", corner_1, corner_2, corner_3, max_payout )
 
   hh_groups = ppl.groupby(['hh_id1', 'hh_id2'])
   hhs = (        hh_groups[[ "income_g_m", "income+eitc"                         ]].agg('sum')
           .join( hh_groups[[ col for col in ppl.columns if col.startswith('seg') ]].agg('max') )
   )
   
-  hhs, drop, extreme_drop = compute_poverty_gap_change( hhs, "income_g_m", "income+eitc" )
+  hhs, drop_in_billions, extreme_drop_in_billions = add_poverty_gap_change( hhs, "income_g_m", "income+eitc" )
+
+  hhs, poverty_exits, extreme_poverty_exits = add_poverty_exits( hhs, "income_g_m", "income+eitc" )
+
+  print( cost_in_trillions, drop_in_billions, extreme_drop_in_billions, poverty_exits, extreme_poverty_exits )
 
 
 if False: # some data checks.  I eyeballed these and similar ones; they look good.
@@ -44,3 +48,6 @@ if False: # some data checks.  I eyeballed these and similar ones; they look goo
 
   x = hhs[ hhs["seg-2"] > 0 ]
   x[['income_g_m', 'income+eitc', 'poverty_0', 'poverty_1', 'poverty_drop']]
+
+  x = hhs[ hhs["seg-1"] > 0 ]
+  x[['income_g_m', 'income+eitc', 'poverty_exit', 'extreme_poverty_exit']]
