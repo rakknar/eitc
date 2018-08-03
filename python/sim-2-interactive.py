f06d4164792dd977d257d540685fcde268646e26
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2018-08-03 15:01:28 -0500

    test the household size-scaling of poverty thresholds. (it works.)

--- python/sim-2-interactive.py
+++ python/sim-2-interactive.py
@@ -33,22 +33,30 @@ if True: # results
           .join( hh_groups[[ col for col in ppl.columns if col.startswith('seg') ]].agg('max') )
   )
   
-  hhs, drop_in_billions, extreme_drop_in_billions = add_poverty_gap_change( hhs, "income_g_m", "income+eitc" )
+  hhs, drop_in_billions, extreme_drop_in_billions = add_poverty_gap_change(
+    hhs, "income_g_m", "income+eitc", "persons" )
 
-  hhs, poverty_exits, extreme_poverty_exits = add_poverty_exits( hhs, "income_g_m", "income+eitc", "persons" )
+  hhs, poverty_exits, extreme_poverty_exits = add_poverty_exits(
+    hhs, "income_g_m", "income+eitc", "persons" )
 
   print( cost_in_trillions, drop_in_billions, extreme_drop_in_billions, poverty_exits, extreme_poverty_exits )
 
 
 if False: # some data checks.  I eyeballed these and similar ones; they look good.
-  pd.set_option('display.max_columns', 20) # If on a small screen, futz with this
+  if True: # Display options: These are clearly wrong, but they seem to work.
+    pd.set_option('display.max_columns', 40)
+    pd.set_option('display.width', 1000)
 
   x = hhs[ (hhs["extreme_poverty_0"] > 0) & (hhs["income_g_m"] > 0) ]
   ( x[ x["poverty_0"] > 0]
     [['income_g_m', 'income+eitc', 'poverty_0', 'poverty_1', 'poverty_drop']] )
 
-  x = hhs[ hhs["seg-2"] > 0 ]
-  x[['income_g_m', 'income+eitc', 'poverty_0', 'poverty_1', 'poverty_drop']]
+  x = hhs[ hhs["seg-2"] > 0 ].rename( columns = {"extreme_poverty_drop":"extreme_drop",
+                                                 "extreme_poverty_0":"extreme_0",
+                                                 "extreme_poverty_1":"extreme_1"
+                                     } )
+  x[['income_g_m', 'income+eitc', 'poverty_0', 'poverty_1', 'poverty_drop', 'persons']]
+  x[['income_g_m', 'income+eitc', 'extreme_0', 'extreme_1', 'extreme_drop', 'persons']]
 
-  x = hhs[ hhs["seg-1"] > 0 ]
-  x[['income_g_m', 'income+eitc', 'poverty_exit', 'extreme_poverty_exit']]
+  x = hhs[ hhs["seg-1"] > 0 ].rename( columns = {"extreme_poverty_exit":"extreme_exit"} )
+  x[['income_g_m', 'income+eitc', 'poverty_exit', 'extreme_exit', 'persons']]
