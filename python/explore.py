18dd65e005adec650871781b2f60b46cbb27cf5f
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2018-08-03 18:09:45 -0500

    discover a zero|missing value mystery

--- python/explore.py
+++ python/explore.py
@@ -1,10 +1,11 @@
-import pandas as pd
+# First run main.py
 
-df = pd.read_csv("data/2016-wages.csv")
+if True: # Why are most nonpositive values missing, but a few are zero?
+  len( raw[ raw['wage_g_m']   ==0 ] )
+  len( raw[ raw["profit_g_m"] ==0 ] )
 
-print("About 80% of these values are missing.")
-for x in df.columns:
-  print( x )
-  print( df[x].count() )
-  print( len(df[x]) )
-  print("")
+print( "missing data fractions: " )
+for (name,df) in [("ppl", ppl), ("hhs",hhs)]:
+  print( name + ":")
+  print( df["income_g_m"].count() / len( df["income_g_m"] ) )
+  print()
