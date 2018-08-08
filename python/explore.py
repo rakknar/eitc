635b5a95b97d86ce5b50f6b89c27d5c15ae6bb47
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2018-08-08 11:08:19 -0500

    org mode, missing values exploration

--- python/explore.py
+++ python/explore.py
@@ -1,8 +1,10 @@
 # First run main.py
 
 if True: # Why are most nonpositive values missing, but a few are zero?
-  len( raw[ raw['wage_g_m']   ==0 ] )
-  len( raw[ raw["profit_g_m"] ==0 ] )
+  len( raw[ raw['wage_g_m']   ==0      ] )
+  len( raw[ raw['wage_g_m'].isnull()   ] )
+  len( raw[ raw["profit_g_m"] ==0      ] )
+  len( raw[ raw["profit_g_m"].isnull() ] )
 
 print( "missing data fractions: " )
 for (name,df) in [("ppl", ppl), ("hhs",hhs)]:
